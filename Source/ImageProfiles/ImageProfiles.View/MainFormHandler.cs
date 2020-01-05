67ff90c00a0e5db05717d3c315062f1feaecfebf
baonguyen96 <baonguyendrump@gmail.com>
2020-01-05 13:23:52 -0600

    UI shows progress

--- Source/ImageProfiles/ImageProfiles.View/MainFormHandler.cs
+++ Source/ImageProfiles/ImageProfiles.View/MainFormHandler.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 using ImageProfiles.Representations;
+// ReSharper disable LocalizableElement
 
 namespace ImageProfiles.View
 {
@@ -46,12 +47,18 @@ namespace ImageProfiles.View
 
 		private void Execute(object sender, EventArgs eventArgs)
 		{
+			var start = DateTime.Now;
+			OutputTextbox.Text = $"Start:    {start}\r\n\r\n";
+
 			try
 			{
 				var directoryInfo = new DirectoryInfo(RootDirectoryInput.Text);
 				var directories = ImageProfiles.Program.GetOriginalDirectories(directoryInfo);
-				var modeAsString = OutputModes.Controls.OfType<RadioButton>().First(button => button.Checked).Text.Replace(" ", "");
-				var mode = (RepresentationFactory.RepresentationMode) Enum.Parse(typeof(RepresentationFactory.RepresentationMode), modeAsString);
+				var modeAsString = OutputModes.Controls.OfType<RadioButton>()
+					.First(button => button.Checked)
+					.Text.Replace(" ", "");
+				var mode = (RepresentationFactory.RepresentationMode) 
+					Enum.Parse(typeof(RepresentationFactory.RepresentationMode), modeAsString);
 				var representation = RepresentationFactory.GetRepresentation(mode);
 				var step = 0;
 				var maxLength = directories.Count.ToString().Length;
@@ -59,21 +66,33 @@ namespace ImageProfiles.View
 
 				ProgressCounter.Visible = true;
 				ProgressBar.Visible = true;
+				ProgressBar.Value = 0;
 				ProgressBar.Maximum = directories.Count;
 
 				foreach (var directory in directories)
 				{
+					OutputTextbox.Text += $"{directory.FullName}\r\n";
+					OutputTextbox.Refresh();
+
 					ImageProfiles.Program.ProcessDirectory(directory, representation);
 
 					step++;
+					ProgressCounter.Text = $"[{step.ToString($"D{maxLength}")} / {size.ToString($"D{maxLength}")}]";
+					ProgressCounter.Refresh();
 					ProgressBar.Increment(1);
-					
-					ProgressCounter.Text = $@"[{step.ToString($"D{maxLength}")} / {size.ToString($"D{maxLength}")}]";
 				}
 			}
 			catch (Exception exception)
 			{
 				HandleException(null, exception);
+			}
+			finally
+			{
+				var end = DateTime.Now;
+				var duration = end - start;
+
+				OutputTextbox.Text += $"\r\nEnd:      {end}\r\n";
+				OutputTextbox.Text += $"Run time: {duration.Hours:D2}:{duration.Minutes:D2}:{duration.Seconds:D2}";
 			}
 		}
 	}
